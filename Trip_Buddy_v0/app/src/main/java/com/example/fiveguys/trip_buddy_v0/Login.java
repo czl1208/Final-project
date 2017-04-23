16d0571d2549c57cd7d64d9bafd228baef443f4c
czl1208 <czl1208@bu.edu>
2017-04-23 15:33:11 -0400

    Facebook

--- Trip_Buddy_v0/app/src/main/java/com/example/fiveguys/trip_buddy_v0/Login.java
+++ Trip_Buddy_v0/app/src/main/java/com/example/fiveguys/trip_buddy_v0/Login.java
@@ -2,6 +2,7 @@ package com.example.fiveguys.trip_buddy_v0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
 
 public class Login extends AppCompatActivity {
 
