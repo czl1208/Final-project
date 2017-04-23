fa4fd67d50f1b48a0e9d503ad2fcf564a210b5a8
czl1208 <czl1208@bu.edu>
2017-04-23 15:19:02 -0400

    By cao

--- Trip_Buddy_v0/app/src/main/java/com/example/fiveguys/trip_buddy_v0/Login.java
+++ Trip_Buddy_v0/app/src/main/java/com/example/fiveguys/trip_buddy_v0/Login.java
@@ -1,8 +1,13 @@
 package com.example.fiveguys.trip_buddy_v0;
 
-/**
- * Created by Victor on 4/23/17.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Login {
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
--- Trip_Buddy_v0/app/src/main/java/com/example/fiveguys/trip_buddy_v0/NewTrip.java
+++
@@ -1,8 +0,0 @@
-package com.example.fiveguys.trip_buddy_v0;
-
-/**
- * Created by Victor on 4/23/17.
- */
-
-public class NewTrip {
-}
